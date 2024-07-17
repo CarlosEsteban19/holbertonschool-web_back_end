2083abc9618669b1142e18de42eba284ff6dae79
Charlie <carlos.esteban19@outlook.com>
2024-07-17 16:13:34 -0400

    .

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [idx, value] of array.entries()) {
+  for (const value of array) {
+    const idx = array.indexOf(value);
     array[idx] = appendString + value;
   }
-
   return array;
 }
