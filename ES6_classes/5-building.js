63abc17c4791347499c481b751471a62b634e93b
Charlie <carlos.esteban19@outlook.com>
2024-07-18 15:07:29 -0400

    new target

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
+    if (new.target === Building) {
       throw new Error('Building is an abstract class and cannot be instantiated directly.');
     }
     this._sqft = sqft;
