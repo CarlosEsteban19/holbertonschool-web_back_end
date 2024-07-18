895499674d3349682cf85b0380e1e7a4bcfa70d9
Charlie <carlos.esteban19@outlook.com>
2024-07-18 15:53:55 -0400

    qsy`

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,22 +1,12 @@
-// 5-building.js
-class Building {
+export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    if (new.target === Building) {
-      throw new Error('Cannot instantiate abstract class Building');
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  // Désactivation de la règle ESLint pour cette méthode spécifique
-  /* eslint-disable class-methods-use-this */
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
-  /* eslint-enable class-methods-use-this */
 }
-
-export default Building;
