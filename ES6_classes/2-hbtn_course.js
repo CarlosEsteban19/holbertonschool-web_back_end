9571c5710b9361f9cf4fdc294fb1294f90940d3e
Charlie <carlos.esteban19@outlook.com>
2024-07-18 13:43:58 -0400

    fix?

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,11 +1,15 @@
 export default class HolbertonCourse {
-  /**
-     *
-     * @param {String} name
-     * @param {Number} length
-     * @param {Array} students
-     */
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    }
+
     this._name = name;
     this._length = length;
     this._students = students;
