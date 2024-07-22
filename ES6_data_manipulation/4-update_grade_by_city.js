746525ec7615962d52843dd8cd9a8e78027866ad
Charlie <carlos.esteban19@outlook.com>
2024-07-22 14:53:51 -0400

    find

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(list, city, newGrades) {
   return list
     .filter((student) => student.location === city)
     .map((student) => {
-      const studentGrade = newGrades.filter((grade) => grade.studentId === student.id);
+      const studentGrade = newGrades.find((grade) => grade.studentId === student.id);
       return { ...student, grade: studentGrade ? studentGrade.grade : 'N/A' };
     });
 }
