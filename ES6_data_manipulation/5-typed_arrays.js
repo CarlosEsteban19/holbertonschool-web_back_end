24a8f40105530cc077cfa6fe70dec866943c7ce5
Charlie <carlos.esteban19@outlook.com>
2024-07-22 15:52:51 -0400

    ret array

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,10 @@
 export default function createInt8TypedArray(lenght, position, value) {
-    if (lenght <= position || position < 0) {
-      throw new Error('Position outside range');
-    }
+  if (lenght <= position || position < 0) {
+    throw new Error('Position outside range');
+  }
   const buffer = new ArrayBuffer(lenght);
   const array = new Int8Array(buffer);
 
   array[position] = value;
-  return buffer;
+  return array;
 }
