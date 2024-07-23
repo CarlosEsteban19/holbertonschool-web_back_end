ad52253f262f6972dd287b2a4d6cb79adcdfd242
Charlie <carlos.esteban19@outlook.com>
2024-07-23 12:24:17 -0400

    edgecase

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
+  if (!startString || typeof startString !== 'string') return '';
   return Array.from(set)
     .filter((item) => item.startsWith(startString))
     .map((item) => item.slice(startString.length))
