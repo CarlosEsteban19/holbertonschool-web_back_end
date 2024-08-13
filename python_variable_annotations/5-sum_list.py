75c7db7c98f554ba59c5d3b12275c127c3a25682
Charlie <carlos.esteban19@outlook.com>
2024-08-13 12:22:41 -0400

    sum list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""sum list of stuff"""
+
+
+def sum_list(imput_list: list[float]) -> float:
+    """hola mami"""
+    result: float = 0
+    for num in imput_list:
+        result += num
+    return result
