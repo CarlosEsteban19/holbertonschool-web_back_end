b6e2d9e25df17204bee93a815ff2640261249263
Charlie <carlos.esteban19@outlook.com>
2024-08-13 12:39:52 -0400

    to kv

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""mi codigo se ve bn"""
+from typing import Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> tuple:
+    """tuple y su madre"""
+    result: float = v * v
+    return (k, result)
