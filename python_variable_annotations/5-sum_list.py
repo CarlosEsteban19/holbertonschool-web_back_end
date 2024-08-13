6fe98f444af1b2e51f7b82fedfabca460553ba74
Charlie <carlos.esteban19@outlook.com>
2024-08-13 12:25:03 -0400

    typing

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """sum list of stuff"""
+from typing import List
 
 
-def sum_list(imput_list: list[float]) -> float:
+def sum_list(imput_list: List[float]) -> float:
     """hola mami"""
     result: float = 0
     for num in imput_list:
