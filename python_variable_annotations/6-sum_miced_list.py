76e6a8c1ee48ce7bddc8a1c82ec71f3004f00ba4
Charlie <carlos.esteban19@outlook.com>
2024-08-13 12:27:49 -0400

    mxd list

---
+++ python_variable_annotations/6-sum_miced_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""documentando codigo"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List) -> float:
+    """funcion q hace algo"""
+    result: float = 0
+    for num in mxd_lst:
+        result += num
+    return result
