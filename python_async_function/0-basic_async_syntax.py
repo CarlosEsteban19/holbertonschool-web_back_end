112eda9d1372e98901263d2c20635bac4161eede
Charlie <carlos.esteban19@outlook.com>
2024-08-15 11:14:00 -0400

    what

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """comentandoooooo"""
 import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """otro comentariooo"""
-    return await asyncio.random(max_delay)
+    num = random(max_delay)
+    return await asyncio(num)
