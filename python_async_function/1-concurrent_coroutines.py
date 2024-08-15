b82758c7f096f3dd85443f828ced297731d8ee01
Charlie <carlos.esteban19@outlook.com>
2024-08-15 11:54:54 -0400

    typing

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """comentandoooooo"""
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """otro comentario"""
     delays = []
 
