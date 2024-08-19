ae5839c25a1dd40f172be4e1e0e42f537d17f308
Charlie <carlos.esteban19@outlook.com>
2024-08-19 15:17:38 -0400

    esta lista flota

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """1-async_comprehension.py"""
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """1-async_comprehension.py"""
     return [number async for number in async_generator()]
