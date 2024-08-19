bb1d6a17dbeadfae90fd4a5d1e487b4d21bd9235
Charlie <carlos.esteban19@outlook.com>
2024-08-19 14:03:24 -0400

    uniform

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,4 +8,4 @@ async def async_generator():
     """0-async_generator.py"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
