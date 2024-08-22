b4fc548dbf6a99826ab0e44145179997df7739e1
Charlie <carlos.esteban19@outlook.com>
2024-08-22 17:25:13 -0400

    less comprehension

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """task 8"""
-from typing import List
 from pymongo import MongoClient
 
 
-def list_all(mongo_collection) -> List:
+def list_all(mongo_collection):
     """list all docs"""
     client = MongoClient('mongodb://localhost:27017/')
     db = client['my_db']
     collection = db[mongo_collection]
     documents = collection.find()
-
-    return [doc for doc in documents]
+    docs = []
+    for doc in documents:
+        docs.append(doc)
+    return docs
