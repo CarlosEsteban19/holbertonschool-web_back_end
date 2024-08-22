274e905a7b59c90d010ff2f8c8d6622fb200f357
Charlie <carlos.esteban19@outlook.com>
2024-08-22 18:26:00 -0400

    11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""task 11"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """school topic"""
+    results = list(mongo_collection.find({'topic': topic}))
+    return results
