fdc1933c6826d96a55d55c9803eeef4530b7171c
Charlie <carlos.esteban19@outlook.com>
2024-08-22 17:27:11 -0400

    asi?

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python3
 """task 8"""
-from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
     """list all docs"""
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client['my_db']
-    collection = db[mongo_collection]
-    documents = collection.find()
-    docs = []
-    for doc in documents:
-        docs.append(doc)
+    docs = list(mongo_collection.find())
     return docs
